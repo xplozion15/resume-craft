0ccf8b5d079c6889bbfa5fbf920ddb0a51bb8d14
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-05-13 13:45:47 +0530

    feat: make landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,20 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <p className="logo">Resume<span>Craft</span></p>
+
+        <div className="hero-section">
+            <h1>Resume<span>Craft</span>: Your Resume, Done Right – Fast and Easy.</h1>
+            <p className="description-paragraph">Create a standout resume effortlessly, with a simple and smooth process that saves you time and lets you focus on what matters most—landing your dream job!</p>
+            <button>GET STARTED</button>
+        </div>
     </>
   );
 }
